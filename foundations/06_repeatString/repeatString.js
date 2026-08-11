fffea01c36a998e6c93028a724682cbe2c2ebe34
alchemist <284885967+thisisalchemist@users.noreply.github.com>
2026-08-11 14:54:16 +0530

    06 and 07 done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR"
+    }
+    
+    let repeatString = '';
 
+    for(let i = 0;i < num; i++){
+        repeatString = str + repeatString;
+    }
+    return repeatString
 };
 
 // Do not edit below this line
