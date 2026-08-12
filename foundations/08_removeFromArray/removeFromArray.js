b659f904187f6713103939189764bbe30eb425e3
alchemist <284885967+thisisalchemist@users.noreply.github.com>
2026-08-12 06:37:01 +0530

    done upto tempconversion

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    for(let i = 1; i <= arguments.length; i++){
+        while(arguments[0].includes(arguments[i])){
+            let removeIndex = arguments[0].indexOf(arguments[i])
+            arguments[0].splice(removeIndex,1)
+        }
+    }
+    return arguments[0]
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR"
+    }
+    if(start > end){
+        let num = start;
+        start = end
+        end = num
+    }
+    while(start <= end){
+        sum += start
+        start++;
+    }
+    return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
